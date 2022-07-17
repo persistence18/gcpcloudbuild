0e2c03ae2e9211d8d7a82e073872dfffd869b4e5
merlock18 <persistence18@gmail.com>
2022-07-17 23:08:43 +0000

    add a new api route

--- main.py
+++ main.py
@@ -32,6 +32,11 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+@app.route("/bob")
+def bob():
+    value = {"value" : "bob"}
+    return jsonify(value)
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
